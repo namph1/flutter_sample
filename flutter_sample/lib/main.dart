dd1253a9c02ba109c1887d7116f26d58855772c0
namph1 <phamhuynam.1990@gmail.com>
2018-08-12 09:50:01 +0700

    lan 1: login websocket and home with menu drawer

---
+++ flutter_sample/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static final channel = IOWebSocketChannel.connect("ws://192.168.95.2:5001");
+
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(
+          channel: channel,
+        ),
+    HomePage.tag: (context) => HomePage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(channel: channel),
+      routes: routes,
+    );
+  }
+}
